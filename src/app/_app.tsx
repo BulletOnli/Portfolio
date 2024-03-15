447bddab0664017d5fbfface6467134e274296c5
Gemmuel <gemmueldelacruz@gmail.com>
2024-03-15 23:25:17 +0800

    add: vercel insights

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -2,16 +2,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const App = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <>
-            <CacheProvider>
-                <ChakraProvider>{children}</ChakraProvider>
-            </CacheProvider>
-            <Toaster />
-        </>
-    );
+  return (
+    <>
+      <CacheProvider>
+        <ChakraProvider>{children}</ChakraProvider>
+      </CacheProvider>
+      <Toaster />
+      <SpeedInsights />
+    </>
+  );
 };
 
 export default App;
