31d1c66fa4a92aada2092868f68dd80a6e61441b
Gemmuel <gemmueldelacruz@gmail.com>
2024-07-21 18:33:31 +0800

    fix sitemap date

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ const PUBLIC_ROUTES = ["", "/about", "/contact", "/projects"];
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const publicRoutes: MetadataRoute.Sitemap = PUBLIC_ROUTES.map((route) => ({
     url: process.env.NEXT_PUBLIC_SERVER_URL + route,
-    lastModified: "2024-07-019T00:00:00.000Z",
+    lastModified: "2024-07-19T00:00:00.000Z",
     changeFrequency: "monthly",
     priority: route === "" ? 1 : 0.5,
   }));
